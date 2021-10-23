746f77f19ac2d1eea912b643e751c47c158de543
jszostakk <jakub.szostakk@gmail.com>
2021-10-23 15:08:53 +0200

    update

---
+++ Lab2/zestaw3/cmake-build-debug/CMakeFiles/zestaw3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zestaw3.dir/src/main.cpp.obj"
+  "libzestaw3.dll.a"
+  "zestaw3.exe"
+  "zestaw3.exe.manifest"
+  "zestaw3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zestaw3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
