69861ad54cc5f6eafe264884712188ba3b24a404
jszostakk <jakub.szostakk@gmail.com>
2021-11-08 14:00:03 +0100

    lab5

---
+++ Lab5/zestaw1/include/Pojazd.h
@@ -0,0 +1,8 @@
+//
+// Created by student on 08.11.21.
+//
+
+#ifndef PROJECT_POJAZD_H
+#define PROJECT_POJAZD_H
+
+#endif //PROJECT_POJAZD_H
---
+++ Lab5/zestaw1/src/Pojazd.cpp
@@ -0,0 +1,4 @@
+//
+// Created by student on 08.11.21.
+//
+
